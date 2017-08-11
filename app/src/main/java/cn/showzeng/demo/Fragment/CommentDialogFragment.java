ee5221040283dca7fb814645d44f8708e219751f
showzeng <694891221@qq.com>
2017-08-11 23:46:17 +0800

    set EditText cursor to fix strange perform on some smartphones especially like meizu

--- app/src/main/java/cn/showzeng/demo/Fragment/CommentDialogFragment.java
+++ app/src/main/java/cn/showzeng/demo/Fragment/CommentDialogFragment.java
@@ -81,6 +81,7 @@ public class CommentDialogFragment extends DialogFragment implements View.OnClic
     private void fillEditText() {
         dataCallback = (DialogFragmentDataCallback) getActivity();
         commentEditText.setText(dataCallback.getCommentText());
+        commentEditText.setSelection(dataCallback.getCommentText().length());
         if (dataCallback.getCommentText().length() > 0) {
             sendButton.setEnabled(true);
             sendButton.setClickable(true);
